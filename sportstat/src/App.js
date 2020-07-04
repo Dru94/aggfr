c1031db8592fd99ba3c8197b9e211b95e49363e9
Andrew Musali <andrewmusali@gmail.com>
2020-07-04 21:57:18 +0300

    creates project structure and adds navbar

---
+++ sportstat/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TopBar from './containers/topbar.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ sportstat/src/containers/topbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { InputGroup, Navbar, FormControl, Form, Nav } from 'react-bootstrap';
+import {FaSearch} from 'react-icons/fa'
+import './topbar.css';
+
+function TopBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+	  <Navbar.Brand href="#home">Sports Center</Navbar.Brand>
+	  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+	  <Navbar.Collapse id="basic-navbar-nav">
+	    <Nav id='hnavlink' className="ml-auto">
+	      <Nav.Link  href="#home">Home</Nav.Link>
+	    </Nav>
+	    <Form inline>
+	      <InputGroup className="">
+		    <FormControl
+		      placeholder="Search"
+		      aria-label="Search"
+		      aria-describedby="basic-addon2"
+		    />
+		    <InputGroup.Append>
+		      <InputGroup.Text id="basic-addon2"><FaSearch/></InputGroup.Text>
+		    </InputGroup.Append>
+		  </InputGroup>
+	    </Form>
+	  </Navbar.Collapse>
+	</Navbar>
+  );
+}
+
+export default TopBar;
