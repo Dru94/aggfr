f9d1b4743461e48df69d082d4ee777ef8766b9f4
Andrew Musali <andrewmusali@gmail.com>
2020-10-18 23:54:47 +0300

    Updates

---
+++ precache-manifest.a930af80741297f51531002640d5c415.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c1acc5c5bcb94e5f76ae82159e74e5",
+    "url": "/aggfr/index.html"
+  },
+  {
+    "revision": "9075ce885c5fb140f32c",
+    "url": "/aggfr/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d5fec71a0d51df896098",
+    "url": "/aggfr/static/css/main.e9e11631.chunk.css"
+  },
+  {
+    "revision": "9075ce885c5fb140f32c",
+    "url": "/aggfr/static/js/2.98c35442.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/aggfr/static/js/2.98c35442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5fec71a0d51df896098",
+    "url": "/aggfr/static/js/main.2a1491e3.chunk.js"
+  },
+  {
+    "revision": "07a26952701ab7861182",
+    "url": "/aggfr/static/js/runtime-main.6349d758.js"
+  },
+  {
+    "revision": "9b41a5652900f6a296f7c3c4109947f6",
+    "url": "/aggfr/static/media/person.9b41a565.jpg"
+  }
+]);
